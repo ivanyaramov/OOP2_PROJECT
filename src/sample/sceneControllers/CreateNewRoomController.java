536e921370c8b61a1dd3542839a37e9d8739a351
Mustafa <mustafa.mustafov@smart-soft.net>
2022-11-26 15:40:23 +0200

    prep for merge

--- src/sample/sceneControllers/CreateNewRoomController.java
+++ src/sample/sceneControllers/CreateNewRoomController.java
@@ -5,8 +5,13 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import sample.models.DTOs.RoomDTO;
+import sample.models.hotels.RoomCategory;
+import sample.services.RoomService;
 import sample.services.UserService;
+import sample.services.impl.RoomServiceImpl;
 import sample.services.impl.UserServiceImpl;
+import sample.utilities.RedirectScenes;
 
 import java.io.IOException;
 
@@ -23,6 +28,19 @@ public class CreateNewRoomController {
 
     @FXML
     public void createNewRoom(ActionEvent event) throws IOException {
-
+        RoomService roomService = new RoomServiceImpl();
+        if(!roomService.roomNumberExists(Integer.parseInt(roomNumberFXML.getText())))
+        {
+            RoomDTO roomDTO = new RoomDTO();
+            roomDTO.setNumber(Integer.parseInt(roomNumberFXML.getText()));
+            roomDTO.setRoomCategory(RoomCategory.valueOf((String) typeOfRoomFXML.getValue()));
+            roomDTO.setPricePerNight(Double.parseDouble(priceOfRoomFXML.getText()));
+            roomService.createRoom(roomDTO);
+            RedirectScenes.redirect(event, "createNewHotel");
+        }else
+        {
+            roomNumberTakenLabelFXML.setText("Room number is already taken");
+            return;
+        }
     }
 }
