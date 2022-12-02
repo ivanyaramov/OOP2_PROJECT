64150a4b72bc4f5971979e6e36d4dd384f0572ca
Ivan Yaramov <ivan.yaramov@gmail.com>
2022-12-02 16:57:06 +0200

    to hotel view

--- src/sample/services/HotelService.java
+++ src/sample/services/HotelService.java
@@ -2,11 +2,12 @@ package sample.services;
 
 import sample.models.DTOs.HotelDTO;
 import sample.models.hotels.Hotel;
+import sample.models.viemModels.HotelView;
 
 import java.util.List;
 
 public interface HotelService {
     void createHotel(HotelDTO hotelDTO);
     Hotel getHotelById(Long id);
-    List<Hotel> getAllHotels();
+    List<HotelView> getAllHotels();
 }
