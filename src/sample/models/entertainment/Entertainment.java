7059d644c487a7296a370ff0b905362332237567
Ivan Yaramov <ivan.yaramov@gmail.com>
2022-12-11 16:48:58 +0200

    added id getter for entertainment

--- src/sample/models/entertainment/Entertainment.java
+++ src/sample/models/entertainment/Entertainment.java
@@ -54,4 +54,12 @@ public class Entertainment {
     public void setHotel(Hotel hotel) {
         this.hotel = hotel;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
