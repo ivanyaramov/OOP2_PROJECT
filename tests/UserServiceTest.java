b4c1cd328d6e26e296103854b211a1ac12cd05ea
Ivan Yaramov <ivan.yaramov@gmail.com>
2023-01-15 14:08:44 +0200

    writing tests

--- tests/UserServiceTest.java
+++ tests/UserServiceTest.java
@@ -8,9 +8,14 @@ import sample.models.DTOs.RegisterDTO;
 import sample.models.people.Gender;
 import sample.models.people.Person;
 import sample.models.people.Role;
+import sample.models.ratings.ClientRating;
+import sample.models.reservations.Reservation;
 import sample.repository.UserRepository;
 import sample.services.UserService;
 import sample.services.impl.UserServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,6 +25,7 @@ public class UserServiceTest {
     UserService userService;
     ModelMapper modelMapper;
     private final String username = "testName";
+    private final String username2 = "test2Name";
 
     @BeforeEach
     public void before(){
@@ -59,4 +65,57 @@ public class UserServiceTest {
         assertNotNull(actualPerson);
         assertEquals(person.getFullName(), actualPerson.getFullName());
     }
+
+    @Test
+    public void getPeopleByUsernameTest(){
+        Person person1 = new Person();
+        person1.setFullName("Ivan Ivanov");
+        Person person2 = new Person();
+        person1.setFullName("Dimitar Dimitrov");
+        Mockito.when(userRepository.getPersonByUsername(username)).thenReturn(person1);
+        Mockito.when(userRepository.getPersonByUsername(username2)).thenReturn(person2);
+        List<String> listOfUsernames = new ArrayList<>();
+        listOfUsernames.add(username);
+        listOfUsernames.add(username2);
+        List<Person> list = userService.getPeopleByListOfUsernames(listOfUsernames);
+        Person actualPerson1 = list.get(0);
+        Person actualPerson2= list.get(1);
+        assertNotNull(actualPerson1);
+        assertEquals(person1.getFullName(), actualPerson1.getFullName());
+        assertNotNull(actualPerson2);
+        assertEquals(person2.getFullName(), actualPerson2.getFullName());
+    }
+
+    @Test
+    public void updatePersonRatingTest(){
+        Person person1 = new Person();
+        person1.setFullName("Ivan Ivanov");
+        List<ClientRating> list = new ArrayList<>();
+        ClientRating clientRating1 = new ClientRating(person1, 8, new Reservation());
+        ClientRating clientRating2 = new ClientRating(person1, 4, new Reservation());
+        list.add(clientRating1);
+        list.add(clientRating2);
+        double avg = (clientRating1.getRating() + clientRating2.getRating()) / 2;
+        userService.updatePersonRating(person1, list);
+        ArgumentCaptor<Person> argument = ArgumentCaptor.forClass(Person.class);
+        Mockito.verify(userRepository).update(argument.capture());
+        Assertions.assertEquals(person1.getFullName(), argument.getValue().getFullName());
+        Assertions.assertEquals(avg, argument.getValue().getRating());
+    }
+
+    @Test
+    public void changePersonRoleTest(){
+        Person person1 = new Person();
+        person1.setFullName("Ivan Ivanov");
+        Mockito.when(userRepository.getPersonByUsername(username)).thenReturn(person1);
+        String role = "MANAGER";
+        userService.changePersonRole(username, role);
+        ArgumentCaptor<Person> argument = ArgumentCaptor.forClass(Person.class);
+        Mockito.verify(userRepository).update(argument.capture());
+        Assertions.assertEquals(role, String.valueOf(argument.getValue().getRole()));
+    }
+
+
+
+
 }
